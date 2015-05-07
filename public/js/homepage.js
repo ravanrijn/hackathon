2b486b65e73435d74e61eaddd5dc18b31460acc4
acazacu <acazacu@ebay.com>
2015-05-07 11:36:14 +0200

    Homepage carousel first go.

---
+++ public/js/homepage.js
@@ -0,0 +1,13 @@
+/**
+ * Created by acazacu on 5/7/15.
+ */
+(function($) {
+    $(function() {
+        $('#top-banner-wrapper').slick({
+            //autoplay: true,
+            //autoplaySpeed: 5000,
+            arrows: false,
+            dots: false
+        });
+    });
+})(jQuery);
